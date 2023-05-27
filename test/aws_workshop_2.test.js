fc159fe953e60b15e63a8581422ec25dd134f0c3
eliazzo <bethazz@hotmail.co.uk>
2023-05-27 12:39:21 +0100

    first commit

---
+++ test/aws_workshop_2.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const AwsWorkshop2 = require('../lib/aws_workshop_2-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_workshop_2-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new AwsWorkshop2.AwsWorkshop2Stack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
